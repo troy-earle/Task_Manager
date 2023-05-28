54e22f887a3f64cc2710b4cf1db28962d89e3792
allen <allen.tuazon@gmail.com>
2023-05-28 11:45:03 +1000

    updated files to debug some errors and load main

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ const sess = {
 app.use(session(sess));
 
 // Inform Express.js on which template engine to use
-// app.engine("handlebars", hbs.engine);
+app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
 // Parse request bodies as JSON
