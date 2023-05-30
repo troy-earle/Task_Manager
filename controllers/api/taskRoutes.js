469b66c334d7b45abc27631191a97f913e841450
Karun Thiyagarajah <tknanthan@gmail.com>
2023-05-30 19:28:01 +1000

    testing loging function

--- controllers/api/taskRoutes.js
+++ controllers/api/taskRoutes.js
@@ -4,26 +4,29 @@ const withAuth = require("../../utils/auth");
 
 // Use withAuth middleware to prevent access to route
 //display tasks
-router.get("/", withAuth, async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ["password"] },
-      include: [{ model: Task }],
+    let userId = 4;
+    console.log("userId", userId);
+    const taskData = await Task.findAll({
+      where: {
+        user_id: userId,
+      },
     });
-
-    const user = userData.get({ plain: true });
-
+    console.log(taskData);
+    const tasks = taskData.map((task) => task.get({ plain: true }));
+    console.log(tasks);
+    // console.log(user.tasks[1]);
     res.render("tasklist_with_createtask", {
-      ...user,
-      logged_in: true,
+      tasks,
     });
   } catch (err) {
     res.status(500).json(err);
+    console.log(err);
   }
 });
-
-router.get("/Tasks/:id", withAuth, async (req, res) => {
+router.get("/:id", withAuth, async (req, res) => {
   try {
     const taskData = await Task.findByPk(req.params.id, {
       include: [
@@ -45,11 +48,12 @@ router.get("/Tasks/:id", withAuth, async (req, res) => {
   }
 });
 
-router.post("/", withAuth, async (req, res) => {
+router.post("/", async (req, res) => {
   try {
-    const newTask = await Task.create({
+    console.log("trying post");
+      const newTask = await Task.create({
       ...req.body,
-      user_id: req.session.user_id,
+      //user_id: req.session.user_id,
     });
 
     res.status(200).json(newTask);
@@ -58,12 +62,12 @@ router.post("/", withAuth, async (req, res) => {
   }
 });
 
-router.delete("/:id", withAuth, async (req, res) => {
+router.delete("/:id",  async (req, res) => {
   try {
     const taskData = await Task.destroy({
       where: {
         id: req.params.id,
-        user_id: req.session.user_id,
+        //user_id: req.session.user_id,
       },
     });
 
